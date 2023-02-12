7de269c9b5db8fd67cd4cf358a94d5ec112be74d
tomo <o_tomo_1990@yahoo.co.jp>
2023-02-12 21:33:47 +0900

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "books#index"
+  devise_for :users
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
