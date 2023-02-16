b870941ea0759535cf5649433419db1bbfa176b0
tomo <o_tomo_1990@yahoo.co.jp>
2023-02-16 21:10:39 +0900

    letter_openerの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
   root to: "books#index"
   devise_for :users
   resources :books
   resources :users, only: [:index, :show]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
