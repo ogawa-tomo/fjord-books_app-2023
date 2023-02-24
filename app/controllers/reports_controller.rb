d710388ba438bc398475fef87faede5c67e33cea
tomo <o_tomo_1990@yahoo.co.jp>
2023-02-24 11:51:30 +0900

    日報の更新機能

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,6 +13,7 @@ class ReportsController < ApplicationController
   end
 
   def edit
+    redirect_to report_url(@report), notice: t('controllers.report.cannot_edit_other_user_report') if @report.user != current_user
   end
 
   def create
@@ -21,6 +22,14 @@ class ReportsController < ApplicationController
       redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
