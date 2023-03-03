585435fbf8bfe39e8fd1850528b1c0243154c66c
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-03 20:35:34 +0900

    ユーザー一覧ページのビューを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:created_at, :id).page(params[:page]).per(10)
+    @users = User.order(:created_at, :id).page(params[:page])
   end
 
   def show
