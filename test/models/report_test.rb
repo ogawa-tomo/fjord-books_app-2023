77f5efe4b8fdd72c73c02e2b05c6369203751731
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-09 21:10:03 +0900

    reportモデルのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'editable?' do
+    user = User.new(name: 'hoge', email: 'hoge@example.com')
+    report = user.reports.build()
+    assert report.editable?(user)
+
+    user2 = User.new(name: 'fuga', email: 'fuga@example.com')
+    assert_not report.editable?(user2)
+  end
+
+  test 'created_on' do
+    user = User.create!(name: 'hoge', email: 'hoge@example.com', password: 'password')
+    report = user.reports.create!(title: 'hogehoge', content: 'fugafuga')
+    assert_equal report.created_at.to_date, report.created_on
+  end
 end
