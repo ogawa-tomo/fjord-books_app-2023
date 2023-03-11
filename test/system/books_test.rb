02bc67b02b7d2f87c6e8964621e8084655771ccd
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-12 07:53:41 +0900

    Book作成時のデータをリアルにする

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,7 +4,7 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    @book = books(:cherry_book)
 
     visit root_url
     fill_in 'Eメール', with: 'alice@example.com'
@@ -21,16 +21,19 @@ class BooksTest < ApplicationSystemTestCase
     visit books_url
     click_on '本の新規作成'
 
-    fill_in 'メモ', with: @book.memo
-    fill_in 'タイトル', with: @book.title
+    fill_in 'タイトル', with: 'Ruby超入門'
+    fill_in 'メモ', with: 'わかりやす～い！'
+    fill_in '著者', with: 'igaiga'
     click_on '登録する'
 
     assert_text '本が作成されました。'
-    click_on '本の一覧に戻る'
+    assert_text 'Ruby超入門'
+    assert_text 'わかりやす～い！'
+    assert_text 'igaiga'
   end
 
   test 'should update Book' do
-    visit book_url(@book)
+    click_on 'この本を表示'
     click_on 'この本を編集', match: :first
 
     fill_in 'メモ', with: @book.memo
