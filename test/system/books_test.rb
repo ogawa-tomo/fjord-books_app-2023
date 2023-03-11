be1beeeb9e85fa2d8b4f879e1d867e523b53b031
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-12 08:39:12 +0900

    booksのsystem testを追記

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -15,6 +15,14 @@ class BooksTest < ApplicationSystemTestCase
   test 'visiting the index' do
     visit books_url
     assert_selector 'h1', text: '本の一覧'
+
+    assert_text 'プロを目指す人のためのRuby入門'
+    assert_text '名著ですね！！！'
+    assert_text 'jnchito'
+
+    assert_text '論理哲学論考'
+    assert_text '歴史的名著'
+    assert_text 'Ludwig Wittgenstein'
   end
 
   test 'should create book' do
@@ -27,27 +35,33 @@ class BooksTest < ApplicationSystemTestCase
     click_on '登録する'
 
     assert_text '本が作成されました。'
+
     assert_text 'Ruby超入門'
     assert_text 'わかりやす～い！'
     assert_text 'igaiga'
   end
 
   test 'should update Book' do
-    click_on 'この本を表示'
-    click_on 'この本を編集', match: :first
+    visit book_url(@book)
+    click_on 'この本を編集'
 
-    fill_in 'メモ', with: @book.memo
-    fill_in 'タイトル', with: @book.title
+    fill_in 'タイトル', with: 'プロを目指す人のためのRuby入門 第2版'
+    fill_in 'メモ', with: 'さらに名著になった'
+    fill_in '著者', with: '伊藤淳一'
     click_on '更新する'
 
     assert_text '本が更新されました'
-    click_on '本の一覧に戻る'
+
+    assert_text 'プロを目指す人のためのRuby入門 第2版'
+    assert_text 'さらに名著になった'
+    assert_text '伊藤淳一'
   end
 
   test 'should destroy Book' do
     visit book_url(@book)
-    click_on 'この本を削除', match: :first
+    click_on 'この本を削除'
 
     assert_text '本が削除されました。'
+    assert_no_text 'プロを目指す人のためのRuby入門'
   end
 end
