5175d02bdd361b0cee4953854946666c159d5c5a
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-12 09:30:54 +0900

    reports_controller_testを通す

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -3,8 +3,12 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @report = reports(:one)
+    @report = reports(:alice_report)
+    @user = users(:alice)
+    sign_in(@user)
   end
 
   test 'should get index' do
