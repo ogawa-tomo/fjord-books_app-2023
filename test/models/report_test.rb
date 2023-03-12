4c668e5ce776e61426622f3431719f496e6c0f38
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-12 09:47:15 +0900

    日報のタイトルと内容が空ではいけないテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,6 +3,19 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
+
+  test 'title should not be blank' do
+    user = User.new(name: 'hoge', email: 'hoge@example.com')
+    report = user.reports.build(title: '', content: 'fugafuga')
+    assert report.invalid?
+  end
+
+  test 'content should not be blank' do
+    user = User.new(name: 'hoge', email: 'hoge@example.com')
+    report = user.reports.build(title: 'hogehoge', content: '')
+    assert report.invalid?
+  end
+
   test 'editable?' do
     user = User.new(name: 'hoge', email: 'hoge@example.com')
     report = user.reports.build
